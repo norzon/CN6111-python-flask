80140e058b8945ed1dbcd0344ab7c3c73e89a35b
Alexandros Leontakianakos <alleon20@gmail.com>
2018-11-13 17:02:42 +0200

    Fix navbar and implement register and login page logic

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30)])
+    displayName = StringField('Display Name', validators=[DataRequired(), Length(min=3, max=30)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    rePassword = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    rememberMe = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+# Imports
 from flask import Flask, render_template, url_for
+from forms import LoginForm, RegistrationForm
 import config
 app = Flask(__name__)
 
+# App configuration
 app.config['SECRET_KEY'] = config.secret
 
+# Register index route
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
     return render_template('pages/index.html')
 
+# Register registration route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('pages/register.html', title='Register', form=form)
+
+# Register login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('pages/login.html', title='Login', form=form)
+
+# Run app
 if __name__ == '__main__':
     app.run(debug=True)
